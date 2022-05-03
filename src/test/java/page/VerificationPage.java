14b7b17f3c0ffc2b55108dd027316dde703b14b5
Ilnur Mameshev <imameshev@inbox.ru>
2022-05-03 21:48:58 +0300

    describe classes

--- src/test/java/page/VerificationPage.java
+++ src/test/java/page/VerificationPage.java
@@ -1,4 +1,23 @@
 package page;
 
+import com.codeborne.selenide.SelenideElement;
+import data.DataHelper;
+
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+
 public class VerificationPage {
+    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement buttonVerify = $("[data-test-id=action-verify] input");
+
+    public VerificationPage() {
+        codeField.shouldBe(visible);
+    }
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode){
+        codeField.setValue(verificationCode.getCode());
+        buttonVerify.click();
+        return  new DashboardPage();
+    }
 }
